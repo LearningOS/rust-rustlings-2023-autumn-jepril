fb46363a3223c038e3201c95dfb2a69ac46fc328
jepril <56028236+jepril@users.noreply.github.com>
2023-10-18 01:40:26 +0000

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(mount:i32) ->i32{
+    if mount<=40{
+        return mount * 2;
+    }else{
+        return mount;
+    }
+
+}
 
 // Don't modify this function!
 #[test]
